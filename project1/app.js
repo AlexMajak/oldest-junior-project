fb8bb044f8f333f3fee3a8d35e2d75e41a329ee9
AlexMajak <alexmajak@mail.ru>
2022-02-26 00:29:23 +0500

    added project 1

--- project1/app.js
+++ project1/app.js
@@ -1,11 +1,34 @@
-let myButton = document.querySelector(".my-button");
-let myInput = document.querySelector(".my-input");
-let myHeader = document.querySelector(".header");
+const numberOne = document.querySelector(".number-one");
+const numberTwo = document.querySelector(".number-two");
+const plusButton = document.querySelector(".plus");
+const minusButton = document.querySelector(".minus");
+const divideButton = document.querySelector(".divide");
+const multiplyButton = document.querySelector(".multiply");
+const result = document.querySelector('.result');
+const clearButton = document.querySelector('.clear')
 
-myButton.addEventListener("click", function () {
-  let amount = parseInt(myInput.value);
-  let VAT = 0.15;
-  myHeader.innerText = `Сумма НДС со ${amount} рублей равна ${
-    amount * VAT
-  } рублей`;
-});
+clearButton.addEventListener('click', function () {
+	result.textContent = '';
+	numberOne.value = '';
+	numberTwo.value = '';
+})
+
+plusButton.addEventListener('click', function () {
+	let sum = Number(numberOne.value) + Number(numberTwo.value);
+	result.textContent = `Сумма чисел равна: ${sum}`;
+})
+
+minusButton.addEventListener('click', function () {
+	let minus = Number(numberOne.value) - Number(numberTwo.value);
+	result.textContent = `Разность чисел равна: ${minus}`;
+})
+
+divideButton.addEventListener('click', function () {
+	let divide = Number(numberOne.value) / Number(numberTwo.value);
+	result.textContent = `Частное чисел равно: ${divide}`;
+}
+)
+multiplyButton.addEventListener('click', function () {
+	let multiply = Number(numberOne.value) * Number(numberTwo.value);
+	result.textContent = `Произведение чисел равно: ${multiply}`;
+})
