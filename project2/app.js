668fb2e6073f33924058766fd9ad3bda87cb840f
Alexandr <alexmajak@mail.ru>
2022-03-06 15:08:08 +0300

    added 9 school

--- project2/app.js
+++ project2/app.js
@@ -2,19 +2,19 @@ const numberOne = document.querySelector(".number-one");
 const button = document.querySelector(".button");
 const result = document.querySelector(".result");
 
-button.addEventListener('click', function () {
-	let price = 100;
-	let age = numberOne.value;
+button.addEventListener("click", function () {
+  let price = 100;
+  let age = numberOne.value;
 
-	//Если младьше 8 цена 0
-	if (age < 8) {
-		price = 0;
-		//если младьше 20 лет цена -20 процентов
-	} if (age < 20) {
-		price = price * 0.8;
-	} else {
-		price = price;
-	}
+  //Если младьше 8 цена 0
+  if (age < 8) {
+    price = 0;
+    //если младьше 20 лет цена -20 процентов
+  } else if (age < 20) {
+    price = price * 0.8;
+  } else {
+    price = price;
+  }
 
-	result.innerText = `Цена билета равна ${price}`;
-})
+  result.innerText = `Цена билета равна ${price}`;
+});
--- project3/app.js
+++ project3/app.js
@@ -1,25 +1,45 @@
-let myInput = document.createElement('input');
-myInput.setAttribute('type', 'number');
-myInput.className = 'number-one';
+let myInput = document.createElement("input");
+myInput.setAttribute("type", "number");
+myInput.className = "number-one";
 
-let myButton = document.createElement('button');
-myButton.innerText = 'Показать';
-myButton.className = 'button';
+let myButton = document.createElement("button");
+myButton.innerText = "Показать";
+myButton.className = "button";
 
-let result = document.createElement('p');
-result.innerText = 'Hi';
-result.className = 'result';
+let text = document.createElement("p");
+text.innerText = "Hi, price tickets equals 100 rubles";
+text.className = "text";
 
-let wrapper = document.createElement('div');
-wrapper.className = 'wrapper';
+let result = document.createElement("p");
+result.innerText = "";
+result.className = "result";
 
+let wrapper = document.createElement("div");
+wrapper.className = "wrapper";
 
-let body = document.querySelector('body');
+let body = document.querySelector("body");
 
 body.appendChild(wrapper);
 body.appendChild(myInput);
 body.appendChild(myButton);
 body.appendChild(result);
+body.appendChild(text);
+
 wrapper.appendChild(myInput);
 wrapper.appendChild(myButton);
+wrapper.appendChild(text);
 wrapper.appendChild(result);
+
+myButton.addEventListener("click", function () {
+  let price = 100;
+  let age = myInput.value;
+
+  if (age <= 8) {
+    price = 0;
+  } else if (age < 20) {
+    price = price * 0.8;
+  } else {
+    price = price;
+  }
+  result.innerText = `Цена билета равна ${price} рублей`;
+});
